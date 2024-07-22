ac69788112e406742829bc4082855bfedd26f4c4
Dr K DeviPriya <k.devipriya20@gmail.com>
2024-07-22 14:47:19 +0530

    Add files via upload
    first java

---
+++ FirstEx.java
@@ -0,0 +1,16 @@
+//simple program
+import java.lang.*;
+class FirstEx
+{
+//define main() method
+ public static void   main(String args[])
+	{
+	System.out.println("hello");
+	System.out.println("java");
+        System.out.println("3+2");
+	System.out.println(3+2);//5
+        System.out.println(3>=2);//true
+	}
+}
+
+
